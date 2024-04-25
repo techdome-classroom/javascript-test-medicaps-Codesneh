2fe29b4eb4ad53180fc8114760f7e41531a591be
Sneh Kumar Patidar <snehkumarpatidar75820@gmail.com>
2024-04-25 13:34:12 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -6,7 +6,7 @@ function longestSubstring(s) {
         
         for (let j = 0, i = 0; j < n; j++) {
             if (map.has(s[j])) {
-                i = Math.max(map.get(s[j]), i); // Move i to the next index of repeating character
+                i = Math.max(map.get(s[j]), i); 
             }
             ans = Math.max(ans, j - i + 1);
             map.set(s[j], j + 1); // Put character and its index
