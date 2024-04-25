ab1084dee73ce9847d3b78fa57c65bc0a571754d
Sneh Kumar Patidar <snehkumarpatidar75820@gmail.com>
2024-04-25 13:31:35 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,5 +1,21 @@
 function longestSubstring(s) {
+    function lengthOfLongestSubstring(s) {
+        let n = s.length;
+        let ans = 0;
+        let map = new Map(); // Store character and its index
+        
+        for (let j = 0, i = 0; j < n; j++) {
+            if (map.has(s[j])) {
+                i = Math.max(map.get(s[j]), i); // Move i to the next index of repeating character
+            }
+            ans = Math.max(ans, j - i + 1);
+            map.set(s[j], j + 1); // Put character and its index
+        }
+        return ans;
+    }
     
+    let s = "abcabcbb";
+    console.log(lengthOfLongestSubstring(s))
 }
 
 module.exports = { longestSubstring };
